4f6dc86029aba2f63d4cdec1a61e4fb2d187c6bc
Tom Slutsky <tom.slutsky@gmail.com>
2020-05-03 10:54:40 +0300

    initial commit

---
+++ lib/templates/component/Component.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { View, StyleSheet } from 'react-native';
+
+function __component_name__(props: Props) {
+	return <View style={styles.root}></View>;
+}
+
+export default __component_name__;
+
+const styles = StyleSheet.create({
+	root: { flex: 1 },
+});
